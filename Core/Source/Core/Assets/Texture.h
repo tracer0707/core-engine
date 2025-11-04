1d35563fd8d13c90d46222d5a8afff86f69c8150
tracer0707 <tracer0707@mail.ru>
2025-11-04 16:32:55 +0300

    Texture native id type fix

--- Core/Source/Core/Assets/Texture.h
+++ Core/Source/Core/Assets/Texture.h
@@ -26,7 +26,7 @@ namespace Core
 		int _width = 0;
 		int _height = 0;
 
-		int _nativeId = 0;
+		unsigned int _nativeId = 0;
 
 		TextureFormat _format = TextureFormat::RGBA8;
 
@@ -35,7 +35,7 @@ namespace Core
 		void rescale(int newW, int newH);
 
 	public:
-		int getNativeId() { return _nativeId; }
+		unsigned int getNativeId() { return _nativeId; }
 
 		int getWidth() { return _width; }
 		int getHeight() { return _height; }
