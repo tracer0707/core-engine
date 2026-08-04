03778e9200f262be129427cb05c5bdf9d2efaee8
tracer0707 <tracer0707@mail.ru>
2026-08-05 01:36:27 +0300

    InputInt added

--- Editor/Source/Editor/Controls/InputInt.cpp
+++ Editor/Source/Editor/Controls/InputInt.cpp
@@ -13,7 +13,34 @@ namespace Editor
 	{
 		if (!_visible) return;
 
-		_actualWidth = (_width > 0.0f) ? _width : ImGui::GetContentRegionAvail().x;
-		_actualHeight = ImGui::GetFrameHeightWithSpacing();
+		const ImGuiStyle& style = ImGui::GetStyle();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_Alpha, style.Alpha * _style.opacity);
+
+		if (!_style.enabled)
+		{
+			ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, style.Alpha * 0.35f);
+		}
+
+		ImGui::SetNextItemWidth(_actualWidth);
+		ImGui::InputInt((std::string("##") + _id).c_str(), &_value, _step);
+
+		_actualWidth = (_width > 0.0f) ? _width : 100.0f;
+		_actualHeight = ImGui::GetFontSize() + style.FramePadding.y * 2.0f;
+
+		if (_value != _prevValue && _onValueChanged != nullptr)
+		{
+			_onValueChanged(_value);
+			_prevValue = _value;
+		}
+
+		ImGui::PopStyleVar();
+
+		if (!_style.enabled)
+		{
+			ImGui::PopItemFlag();
+			ImGui::PopStyleVar();
+		}
 	}
 } // namespace Editor
--- Editor/Source/Editor/Controls/InputInt.h
+++ Editor/Source/Editor/Controls/InputInt.h
@@ -6,11 +6,26 @@ namespace Editor
 {
 	class InputInt : public Control
 	{
+		private:
+			int _value = 0;
+			int _prevValue = 0;
+			int _step = 1;
+
+			std::function<void(int)> _onValueChanged = nullptr;
+
 		public:
 			InputInt();
 			virtual ~InputInt();
 
 			virtual ControlType getControlType() const { return ControlType::InputInt; }
 			virtual void update();
+
+			int getValue() const { return _value; }
+			void setValue(int value) { _value = value; }
+
+			int getStep() const { return _step; }
+			void setStep(int value) { _step = value; }
+
+			void setOnValueChanged(std::function<void(int)> value) { _onValueChanged = value; }
 	};
 } // namespace Editor
