87aae61e68bca6278bffc19a962dc1a176ce357c
tracer0707 <tracer0707@mail.ru>
2026-09-10 00:34:34 +0300

    Thumbnail manager wip

--- Editor/Source/Utils/TextureUtils.h
+++ Editor/Source/Utils/TextureUtils.h
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <filesystem>
+
+#include <Core/Shared/String.h>
 
 struct FIBITMAP;
 struct color_quad_u8;
+
+namespace fs = std::filesystem;
+
+namespace Core
+{
+	class Renderer;
+	class FrameBuffer;
+} // namespace Core
 
 namespace Editor
 {
@@ -13,5 +24,6 @@ namespace Editor
 			static FIBITMAP* makeSquare(FIBITMAP* src);
 			static FIBITMAP* rescale(FIBITMAP* src, int newW, int newH);
 			static void copyPixels(std::vector<color_quad_u8>& dst, FIBITMAP* src, int width, int height);
+			static bool saveFrameBuffer(Core::Renderer* renderer, const Core::FrameBuffer* frameBuffer, const fs::path& fileName);
 	};
 } // namespace Editor
